a18f40c7c4f0129c6243d0e37cc9c129caf0afde
paypur <pieceofpaypur@gmail.com>
2024-04-09 16:55:28 -0400

    lowered skeleton z index

--- src/components/skeletons/SkeletonText.tsx
+++ src/components/skeletons/SkeletonText.tsx
@@ -1,6 +1,6 @@
 export function SkeletonText2rem() {
     return (
-        <div className={`flex h-[1.5rem] pt-[0.3rem] pb-[0.2rem]`}>
+        <div className={`flex h-[1.5rem] pt-[0.3rem] pb-[0.2rem] -z-10`}>
             <div className='h-full w-[2rem] bg-gray-800 rounded animate-pulse' />
         </div>
     )
@@ -8,7 +8,7 @@ export function SkeletonText2rem() {
 
 export function SkeletonText4rem() {
     return (
-        <div className={`flex h-[1.5rem] pt-[0.3rem] pb-[0.2rem]`}>
+        <div className={`flex h-[1.5rem] pt-[0.3rem] pb-[0.2rem]  -z-10`}>
             <div className='h-full w-[4rem] bg-gray-800 rounded animate-pulse' />
         </div>
     )
@@ -16,7 +16,7 @@ export function SkeletonText4rem() {
 
 export function SkeletonText27rem() {
     return (
-        <div className='flex h-[1.5rem] pt-[0.3rem] pb-[0.2rem]'>
+        <div className='flex h-[1.5rem] pt-[0.3rem] pb-[0.2rem] -z-10'>
             <div className={`h-full w-[28rem] bg-gray-800 rounded animate-pulse`} />
         </div>
     )
@@ -24,7 +24,7 @@ export function SkeletonText27rem() {
 
 export function SkeletonText40rem() {
     return (
-        <div className='flex h-[1.5rem] pt-[0.3rem] pb-[0.2rem]'>
+        <div className='flex h-[1.5rem] pt-[0.3rem] pb-[0.2rem] -z-10'>
             <div className={`h-full w-[40.625rem] bg-gray-800 rounded animate-pulse`} />
         </div>
     )
